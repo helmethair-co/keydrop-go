57a3eb0ea7b71c89e006455de09e8ad850af7950
Mark Vujevits <mark.vujevits@prezi.com>
2018-05-11 18:12:42 +0200

    node works, it should be able to stop it

--- lib/library.go
+++ lib/library.go
@@ -6,38 +6,50 @@ import (
 	"fmt"
 	"os"
 
+	geth "github.com/ethereum/go-ethereum/mobile"
+
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	geth "github.com/ethereum/go-ethereum/mobile"
 )
+
+//export Node
+var nod *geth.Node
 
 //StartNode - start the Swarm node
 //export StartNode
 func StartNode(configJSON *C.char) *C.char {
-	dir := C.GoString(configJSON) + "/ethereum"
+	fmt.Println("----------- starting node ---------------")
+	dir := C.GoString(configJSON) + "/ethereum/keystore"
 	if _, err := os.Stat(dir); err != nil {
 		if os.IsNotExist(err) {
-			err := os.Mkdir(dir, os.ModeDir)
+			err := os.MkdirAll(dir, 0700)
 			if err != nil {
 				return C.CString("error 1: " + err.Error())
 			}
+		} else {
+			return C.CString("error 1.5: " + err.Error())
 		}
 	}
 
-	ks := geth.NewKeyStore(fmt.Sprintf("%s/keystore", dir), keystore.LightScryptN, keystore.LightScryptP)
-	//	accountManager := accounts.NewManager(ks)
+	ks := geth.NewKeyStore(dir, keystore.LightScryptN, keystore.LightScryptP)
 
 	account, err := ks.NewAccount("test")
+	if err != nil {
+		return C.CString("error 1.7: " + err.Error())
+	}
+
 	config := geth.NewNodeConfig()
 	config.PssEnabled = true
 	config.PssAccount = account.GetAddress().GetHex()
 	config.PssPassword = "test"
-	nod, err := geth.NewNodeWithKeystore(dir, config, ks)
+	nod, err = geth.NewNodeWithKeystore(dir, config, ks)
 	if err != nil {
 		return C.CString("error 2: " + err.Error())
 	}
 	err = nod.Start()
+	fmt.Println("----------- node started ---------------")
+
 	if err != nil {
 		return C.CString("error 3: " + err.Error())
 	}
@@ -63,4 +75,15 @@ func CreateIdentity() *C.char {
 
 	jsonIdentity, _ := json.Marshal(identityMap)
 	return C.CString(string(jsonIdentity))
+}
+
+//StopNode -
+//export StopNode
+func StopNode() *C.char {
+	err := nod.Stop()
+	if err != nil {
+		return C.CString("error stopping node: " + err.Error())
+	}
+	return C.CString("ok")
+
 }
