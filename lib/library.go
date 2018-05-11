984672ceb890650c0f7dd1e63f4c169f1722bcb7
Mark Vujevits <mark.vujevits@prezi.com>
2018-05-11 14:32:56 +0200

    started to implement node start

--- lib/library.go
+++ lib/library.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
 	"C"
 	"fmt"
-//	geth "github.com/ethereum/go-ethereum/mobile"
+	geth "github.com/ethereum/go-ethereum/mobile"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 )
@@ -13,17 +11,29 @@ import (
 //StartNode - start the Swarm node
 //export StartNode
 func StartNode(configJSON *C.char) *C.char {
-	dir, err := ioutil.TempDir("", "test")
+	dir := C.GoString(configJSON)
+	/*if _, err := os.Stat(dir); err != nil {
+		if os.IsNotExist(err) {
+			// file does not exist
+		} else {
+			// other error
+		}
+	}*/
+
+	ks := geth.NewKeyStore(fmt.Sprintf("%s/keystore", dir), keystore.LightScryptN, keystore.LightScryptP)
+//	accountManager := accounts.NewManager(ks)
+
+
+	account, err := ks.NewAccount("test")
+	config := geth.NewNodeConfig()
+	config.PssEnabled = true
+	config.PssAccount = account.Address.Str()
+	config.PssPassword = "test"
+	nod, err := geth.NewNodeWithKeystore(dir, config, ks)
 	if err != nil {
 		return C.CString(err.Error())
 	}
-	defer os.RemoveAll(dir)
-
-	backends := []accounts.Backend{
-		keystore.NewKeyStore(fmt.Sprintf("%s/keystore", dir), keystore.LightScryptN, keystore.LightScryptP),
-	}
-	accountManager := accounts.NewManager(backends...)
-
-//	config := geth.NewNodeConfig()
-	return C.CString(fmt.Sprintf("%v", accountManager))
+	nod.Start()
+	defer nod.Stop()
+	return C.CString(fmt.Sprintf("%v", nod))
 }
